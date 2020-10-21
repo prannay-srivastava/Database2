c8f43c6a3149a6b400216234f7d85a97d7156fa6
Prannay Srivastava <Prannay.Srivastava@shell.com>
2020-10-21 19:21:34 +0530

    Ready to deploy

---
+++ Database2/Security/https___azasstaexnseq00035ewdl.blob.core.windows.net_sqldbauditlogs.sql
@@ -0,0 +1,3 @@
+﻿CREATE DATABASE SCOPED CREDENTIAL [https://azasstaexnseq00035ewdl.blob.core.windows.net/sqldbauditlogs]
+    WITH IDENTITY = N'SHARED ACCESS SIGNATURE';
+
---
+++ Database2/dbo/Tables/Users.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [id]              BIGINT         NULL,
+    [name]            NVARCHAR (MAX) NULL,
+    [username]        NVARCHAR (MAX) NULL,
+    [email]           NVARCHAR (MAX) NULL,
+    [address_street]  NVARCHAR (MAX) NULL,
+    [address_suite]   NVARCHAR (MAX) NULL,
+    [Address_city]    NVARCHAR (MAX) NULL,
+    [Address_zipcode] NVARCHAR (MAX) NULL,
+    [geo_lat]         NVARCHAR (MAX) NULL,
+    [geo_lng]         NVARCHAR (MAX) NULL,
+    [phone]           NVARCHAR (MAX) NULL,
+    [website]         NVARCHAR (MAX) NULL,
+    [compnay_name]    NVARCHAR (MAX) NULL,
+    [company_catchph] NVARCHAR (MAX) NULL,
+    [company_bs]      NVARCHAR (MAX) NULL
+);
+
